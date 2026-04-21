8b7aead81ecddcf7804bdac8d464bddffeae48fe
Edward Ruchevits <ruchevits@gmail.com>
2026-04-21 15:29:05 +0100

    Add disc v0.1.2

--- Formula/disc.rb
+++ Formula/disc.rb
@@ -1,14 +1,14 @@
 class Disc < Formula
   desc "Disc command-line interface"
   homepage "https://github.com/disctech/disc-cli"
-  version "0.1.1"
+  version "0.1.2"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.1/disc-aarch64-apple-darwin.tar.gz"
-    sha256 "ea6bf130898092ac7d6dc5b3ea7ab3c0daa9715d5d62e2ff057d1db5060ae65f"
+    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.2/disc-aarch64-apple-darwin.tar.gz"
+    sha256 "cf48add9e53aed24655b917f41e42ad01a3da0c607864cabd6ade46890940f8a"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.1/disc-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c6f304d191ee0a605c67f7684b4029e215172aaec38090ab3986a85e33d2f34c"
+    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.2/disc-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "40aef4a073afe0de92361499c30b373c93093b245dea13c39633f30755115869"
   else
     odie "Unsupported platform"
   end
