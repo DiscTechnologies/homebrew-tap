73208cca87be64422fb53011d49269498945e815
Edward Ruchevits <ruchevits@gmail.com>
2026-07-27 13:35:21 +0300

    fix: point formula at v0.1.0 assets

--- Formula/disc.rb
+++ Formula/disc.rb
@@ -1,14 +1,14 @@
 class Disc < Formula
   desc "Disc command-line interface"
-  homepage "https://github.com/disctech/disc-cli"
-  version "0.1.2"
+  homepage "https://github.com/DiscTechnologies/disc-cli"
+  version "0.1.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.2/disc-aarch64-apple-darwin.tar.gz"
-    sha256 "cf48add9e53aed24655b917f41e42ad01a3da0c607864cabd6ade46890940f8a"
+    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.0/disc-aarch64-apple-darwin.tar.gz"
+    sha256 "f2d22a76145aac060b7e9a3df827d93f663c1f3c7273bcffd4c7f4197d0382d1"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.2/disc-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "40aef4a073afe0de92361499c30b373c93093b245dea13c39633f30755115869"
+    url "https://github.com/DiscTechnologies/disc-cli/releases/download/v0.1.0/disc-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "db5c1d2aa2ee90aa743caa221c7292d02c1d88adecd9c806a5e77fc85dd25a20"
   else
     odie "Unsupported platform"
   end
